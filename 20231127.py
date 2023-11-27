683934c85da70b087cf3cd5c8c3620f4a3101dd1
xiezhuoguan <122135184+xiezhuoguan@users.noreply.github.com>
2023-11-27 22:20:44 +0800

    Add files via upload
    字符运算

---
+++ 20231127.py
@@ -0,0 +1,14 @@
+a=int(input("请输入第一门成绩："))
+b=int(input("请输入第二门成绩："))
+c=int(input("请输入第三门成绩："))
+
+avsd=(60<a<90)*1+(a<60)*0
+avse=(a>90)*2+(a<60)*0
+bvsd=(60<b<90)*1+(b<60)*0
+bvse=(b>90)*2+(b<60)*0
+cvsd=(60<c<90)*1+(c<60)*0
+cvse=(c>90)*2+(c<60)*0
+
+s=avse+avsd+bvse+bvsd+cvse+cvsd
+print(s)
+
